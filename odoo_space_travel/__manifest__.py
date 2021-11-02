fa3b7c48106ea6373627e887703bd4e241c5b550
szeben <juliusjaeger@gmail.com>
2021-11-02 12:56:44 +0000

    [ADD] Basic Module

---
+++ odoo_space_travel/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Odoo Space Travel',
+    
+    'summary' : """App to manage space travel""",
+    
+    'description' : """
+    App to manage:
+    - Starship properties
+    - Workers
+    - Travel summary
+    """,
+    
+    'author' : 'Julio Jaeger',
+    
+    'website' : 'https://technestudioit.com',
+    
+    'category' : 'Travel',
+    'version' : '0.1',
+    
+    'depends' : ['base'],
+    
+    'data' : [
+        
+    ],
+    
+    'demo' : [
+        
+    ],
+}
