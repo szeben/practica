9340880e9810a9e35566ce6e3490bb28f3836257
szeben <odoo@ampd1.odoo.com>
2021-11-03 18:28:44 +0000

    Model.py edited

--- odoo_space_travel/models/workers.py
+++ odoo_space_travel/models/workers.py
@@ -6,3 +6,13 @@ class Workers(models.Model):
     
     _name = 'space.workers'
     _description = 'Workers Info'
+    
+    name = fields.Char(string = 'Name', required=True)
+    job = fields.Char(string = 'Job', required=True)
+    
+    level = fields.Selection(string = 'Experience', 
+                             selection=[(beginner, Beginner),
+                                       (intermediate, Intermediate),
+                                       (advanced, Advanced)],
+                             copy=False)
+    
