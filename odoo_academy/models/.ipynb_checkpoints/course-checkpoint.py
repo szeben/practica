190e0f0afe1f13d09bd1779699b5df9d9c58b6ba
Your iglesiasja <joseaiglesiasc@.com>
2021-11-17 22:18:18 +0000

    comentario

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -5,3 +5,11 @@ from odoo import models, fields, api
 class Course (models.Model):
     _name='academy.course'
     _description='course info'
+    
+    name=fields.Char(string='title', required=True)
+    description=fields.Text(string='Description')
+    level=fields.Selection(string='level',
+                          selection=[('beginner','Beginner'), ('intermediate','Intermediate'),('advanced','Advanced')],
+                          copy=False)
+    active=fields.Boolean(string='Active',default='True')
+                        
