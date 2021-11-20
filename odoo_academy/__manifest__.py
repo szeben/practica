de8f138d2d675bfa95230e0446a5eac114e6c111
iglesiasja <joseaiglesiasc@gmail.com>
2021-11-20 23:32:11 +0000

    comentario

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -15,7 +15,7 @@
     'version':'0.1',
     'depends':['base'],
     'data':[
-         'security/academy_security.xml', 'security/ir.model.access.csv', 'views/academy_menuitems.xml'
+         'security/academy_security.xml', 'security/ir.model.access.csv', 'views/academy_menuitems.xml','views/course_views.xml'
     ],
     'demo':[
         'demo/academy_demo.xml',
